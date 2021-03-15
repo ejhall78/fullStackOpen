52d7a945c2f7a90446c02c122988d91790642da2
ejhall78 <ejhall78@gmail.com>
2021-03-15 12:16:20 +0000

    Completed parts 1.3 - 1.5

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -2,60 +2,42 @@ import React from 'react'
 
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-
-  const Header = (props) => {
-    return (
-      <div>
-        <p>
-          Course Name: {props.course}
-        </p>
-      </div>
-    )
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
   }
   
-  const Part = (props) => {
-    return (
-      <div>
-        <p>
-          {props.part} has {props.exercises} exercises
-        </p>
-      </div>
-    )
-  }
-  
-  const Content = () => {
-    return (
-      <div>
-        <Part part={part1} exercises={exercises1} />
-        <Part part={part2} exercises={exercises2} />
-        <Part part={part3} exercises={exercises3} />
-      </div>
-    )
-  }
-  
-  const Total = (props) => {
-    return (
-      <div>
-        <p>
-          Total excersises: {props.exercisesTotal}
-        </p>
-      </div>
-    )
-  }
+  const [part1, part2, part3] = course.parts
   
   return (
     <div>
-      <Header course={course} />
-      <Content />
-      <Total exercisesTotal= {exercises1 + exercises2 + exercises3} />
-      </div>
+      <h1>Course Name: {course.name}</h1>
+      <p>
+        {part1.name} contains {part1.exercises} exercises
+      </p>
+      <p>
+        {part2.name} contains {part2.exercises} exercises
+      </p>
+      <p>
+        {part3.name} contains {part3.exercises} exercises
+      </p>
+      <p>
+        Total exercises: {part1.exercises + part2.exercises + part3.exercises}
+      </p>
+    </div>
   )
 }
 
