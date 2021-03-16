d1a0a495a71bf99946b9c4fc07bab54a58030267
ejhall78 <ejhall78@gmail.com>
2021-03-16 10:56:08 +0000

    Revert

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,54 +1,44 @@
-import React, { useState } from 'react' 
-
-const History = (props) => {
-  if (props.allClicks.length === 0) {
-    return (
-      <div>
-        the app is used by pressing the buttons
-      </div>
-    )
-  }
-  return (
-    <div>
-      Button press history: {props.allClicks.join(' ')}
-    </div>
-  )
-  
-}
-
-const Button = ({handleClick, text}) => (
-  <button onClick={handleClick}>
-    {text}
-  </button>
-)
+import React from 'react'
 
 
 const App = () => {
-  
-
-
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-  const [allClicks, setAll] = useState([])
-  
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L'))
-    setLeft(left + 1)
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
   }
-
-  const handleRightClick = () => {
-    setAll(allClicks.concat('R'))
-    setRight(right +1)
-  }
-
-    return (
-      <div>
-      <Button handleClick={handleLeftClick} text='left' />
-      <Button handleClick={handleRightClick} text='right' />
-      <History allClicks={allClicks} />
-      </div>
-    )
   
+  const [part1, part2, part3] = course.parts
+  
+  return (
+    <div>
+      <h1>Course Name: {course.name}</h1>
+      <p>
+        {part1.name} contains {part1.exercises} exercises
+      </p>
+      <p>
+        {part2.name} contains {part2.exercises} exercises
+      </p>
+      <p>
+        {part3.name} contains {part3.exercises} exercises
+      </p>
+      <p>
+        Total exercises: {part1.exercises + part2.exercises + part3.exercises}
+      </p>
+    </div>
+  )
 }
 
 export default App
